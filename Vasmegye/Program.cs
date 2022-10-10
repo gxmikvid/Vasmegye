0c9a7933d5c1da4a8958940c000230e2695e2663
gxmikvid <69439560+gxmikvid@users.noreply.github.com>
2022-10-10 09:08:18 +0200

    v1.0

---
+++ Vasmegye/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Vasmegye {
+    class Program {
+        static bool CdvEll() {
+            return false;
+        }
+
+        static void Main(string[] args) {
+            string[] nyersAdat = File.ReadAllLines(@"" + Environment.CurrentDirectory + "/../../vas.txt");
+            int[,] tisztaAdat = new int[nyersAdat.Length,3];
+            for (int i = 0; i < nyersAdat.Length; i++) {
+                string[] temp = nyersAdat[i].Split('-');
+                for (int j = 0; j < temp.Length; j++) {
+                    tisztaAdat[i, j] = Convert.ToInt32(temp[j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
